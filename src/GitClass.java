b966e93fe828f29e6b2d13c2219d4039a3ce7cee
Montira <poom.niti1456@gmail.com>
2020-10-20 15:12:36 +0700

    First Commit

---
+++ src/GitClass.java
@@ -0,0 +1,9 @@
+public class GitClass {
+    public static void main(String[] args) {
+        sayHello();
+    }
+    
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+}
